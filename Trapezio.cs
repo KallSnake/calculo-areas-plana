18fe299aff6c2f99db1ea2d00de5111428c8d5d1
Luis Carlos da Silva <34668802+KallSnake@users.noreply.github.com>
2019-04-28 18:51:48 -0300

    Add files via upload

---
+++ Trapezio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularFiguraPlanas
+{
+    class Trapezio
+    {
+
+        private double a;
+        private double bmr;
+        private double bmn;
+        private double h;
+
+        public double AreaTrapezio(double bmr, double bmn, double h)
+        {
+
+            return a = ((bmr + bmn) / 2) * h;
+
+        }
+
+    }
+}
