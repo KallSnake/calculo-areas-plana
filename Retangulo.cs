f3066072a1152c41512aeb6aadb686ca32af0ef5
Luis Carlos da Silva <34668802+KallSnake@users.noreply.github.com>
2019-04-28 18:27:18 -0300

    Add files via upload

---
+++ Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularFiguraPlanas
+{
+    class Retangulo
+    {
+
+        private double a;
+        private double b;
+        private double h;
+
+        public double AreaRetangulo(double b, double h)
+        {
+
+            return a = b * h;
+
+        }
+
+
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularFiguraPlanas
+{
+    class Triangulo
+    {
+
+        private double a;
+        private double b;
+        private double h;
+
+        public double AreaTriangulo(double b, double h)
+        {
+
+            return a = (b * h) / 2;
+
+        }
+
+    }
+}
