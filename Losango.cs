5aaf6bee038ec0a399a098e2f7a30298f87eca6a
Luis Carlos da Silva <34668802+KallSnake@users.noreply.github.com>
2019-04-28 19:03:51 -0300

    Add files via upload

---
+++ Losango.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularFiguraPlanas
+{
+    class Losango
+    {
+
+        private double a;
+        private double dmr;
+        private double dmn;
+
+        public double AreaLosango(double dmr, double dmn)
+        {
+
+            return a = ((dmr * dmn) / 2);
+
+        }
+
+
+    }
+}
